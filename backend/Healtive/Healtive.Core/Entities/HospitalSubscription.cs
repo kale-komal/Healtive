dd71712284a3ecc65a7dcdbc0235450e00efb787
Komal Kale <komalkale3013@gmail.com>
2026-07-21 10:59:45 +0530

    Add entities in Healtive.Core

--- backend/Healtive/Healtive.Core/Entities/HospitalSubscription.cs
+++ backend/Healtive/Healtive.Core/Entities/HospitalSubscription.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Healtive.Core.Entities;
 
-namespace Healtive.Core.Entities
+public class HospitalSubscription
 {
-    internal class HospitalSubscription
-    {
-    }
+    public Guid Id { get; set; }
+
+    public Guid HospitalId { get; set; }
+
+    public Guid SubscriptionPlanId { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+
+    public DateTime? TrialEndsOn { get; set; }
+
+    public decimal AmountPaid { get; set; }
+
+    public string PaymentStatus { get; set; } = string.Empty;
+
+    public bool IsActive { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
 }
